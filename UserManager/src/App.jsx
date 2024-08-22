7585a673501287c5fddca32646232c6501b70bf6
Adil Tehranli <adiljt@code.edu.az>
2024-08-22 08:01:58 +0300

    AddedUserGetWithAxios

--- UserManager/src/App.jsx
+++ UserManager/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import User from './components/User/User'
 
 function App() {
   return (
     <>
 
+      <User/>
     </>
   )
 }
---
+++ UserManager/src/components/User/User.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Button } from "@mui/material";
+import axios from "axios";
+
+const API_KEY = "https://jsonplaceholder.typicode.com/users";
+
+const User = () => {
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get(API_KEY);
+        setUsers(response.data);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>№</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Phone</TableCell>
+              <TableCell>Website</TableCell>
+              <TableCell>
+                <Button color="success">Add</Button>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map((user, index) => (
+              <TableRow
+                key={user.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {index + 1}
+                </TableCell>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>{user.phone}</TableCell>
+                <TableCell>{user.website}</TableCell>
+                <TableCell>
+                  <Button color="secondary">Edit</Button>
+                </TableCell>
+                <TableCell>
+                  <Button color="error">Delete</Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+    </div>
+  );
+};
+
+export default User;
